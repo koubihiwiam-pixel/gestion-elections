5a42de06fbab127035ea89b516795b16c45975d6
Wiam koubihi <koubihiwiam@gmail.com>
2026-09-24 22:52:21 +0200

    initial candidates

---
+++ GestionElection.js
@@ -0,0 +1,140 @@
+let candidats = [
+    {
+        cin: "AB123456",
+        nom: "Boushaba",
+        prenom: "Soufiane",
+        partiPolitique: "Indépendant",
+        age: 40,
+        electeurs: []
+    },
+    {
+        cin: "HH190923",
+        nom: "Koubihi",
+        prenom: "Wiam",
+        partiPolitique: "RNI",
+        age: 22,
+        electeurs: ["AB123456","DC635473","HH509352"]
+    },
+    {
+        cin: "HH178356",
+        nom: "El glouani",
+        prenom: "Rabiaa",
+        partiPolitique: "Indépendant",
+        age: 30,
+        electeurs: []
+    },
+    {
+        cin: "AB763894",
+        nom: "El othmani",
+        prenom: "Khalid",
+        partiPolitique: "PAM",
+        age: 33,
+        electeurs: []
+    },
+    {
+        cin: "DC635473",
+        nom: "Mebrouki",
+        prenom: "Abd slam",
+        partiPolitique: "Indépendant",
+        age: 45,
+        electeurs: []
+    },
+    {
+        cin: "SD647583",
+        nom: "Berada",
+        prenom: "Monir",
+        partiPolitique: "PI",
+        age: 29,
+        electeurs: ["HH178356"]
+    },
+    {
+        cin: "DC546738",
+        nom: "El idrissi",
+        prenom: "Fatiha",
+        partiPolitique: "Indépendant",
+        age: 28,
+        electeurs: []
+    },
+    {
+        cin: "HH376290",
+        nom: "Benchakroun",
+        prenom: "Ibrahim",
+        partiPolitique: "PAM",
+        age: 36,
+        electeurs: ["DN278676"]
+    },
+    {
+        cin: "SD364789",
+        nom: "Wadaa",
+        prenom: "Aya",
+        partiPolitique: "RNI",
+        age: 44,
+        electeurs: ["DN674853","DC546738"]
+    },
+    {
+        cin: "HH839462",
+        nom: "Elmajhad",
+        prenom: "Ayoub",
+        partiPolitique: "RNI",
+        age: 19,
+        electeurs: ["HH973527"]
+    },
+    {
+        cin: "DN674853",
+        nom: "Amrani",
+        prenom: "Moad",
+        partiPolitique: "Indépendant",
+        age: 51,
+        electeurs: []
+    },
+    {
+        cin: "DN278676",
+        nom: "Mahmoudi",
+        prenom: "Alae",
+        partiPolitique: "Indépendant",
+        age: 37,
+        electeurs: []
+    },
+    {
+        cin: "CD456372",
+        nom: "bouanani",
+        prenom: "Wiam",
+        partiPolitique: "PAM",
+        age: 20,
+        electeurs: ["DC265396","CN123231"]
+    },
+    {
+        cin: "HH973527",
+        nom: "Benzima",
+        prenom: "Leila",
+        partiPolitique: "Indépendant",
+        age: 25,
+        electeurs: []
+    },
+    {
+        cin: "DC265396",
+        nom: "Alami",
+        prenom: "Maria",
+        partiPolitique: "Indépendant",
+        age: 41,
+        electeurs: []
+    },
+    {
+        cin: "CN123231",
+        nom: "Merzak",
+        prenom: "Mostapha",
+        partiPolitique: "Indépendant",
+        age: 61,
+        electeurs: []
+    },
+    {
+        cin: "HH509352",
+        nom: "Zeroual",
+        prenom: "Youssef",
+        partiPolitique: "Indépendant",
+        age: 48,
+        electeurs: []
+    }
+]
+
+console.log(candidats);
